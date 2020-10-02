15faea4fd152d5d158e9c9ffc664f49a9e008f16
Brendon Henning <brendonhenning@gmail.com>
2020-10-01 21:56:23 -0500

    added Airport and Flight models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.delete_all
+Flight.delete_all
+
+
+DIA = Airport.create({ city: 'Denver',      code: 'DIA' })
+LNK = Airport.create({ city: 'Lincoln',     code: 'LNK' })
+OMA = Airport.create({ city: 'Omaha',       code: 'OMA' })
+KCI = Airport.create({ city: 'Kansas City', code: 'KCI' })
+ATL = Airport.create({ city: 'Atlanta',     code: 'ATL' })
+
+Flight.create([{ from_airport: LNK, to_airport: DIA,
+                departure_time: '2020-10-02 06:33:00', duration: '01:38:00' },
+               { from_airport: OMA, to_airport: ATL,
+                departure_time: '2020-10-31 08:12:00', duration: '02:52:00' },
+               { from_airport: ATL, to_airport: KCI,
+                departure_time: '2020-10-31 12:15:00', duration: '02:44:00' },
+               { from_airport: LNK, to_airport: KCI,
+                departure_time: '2020-12-15 05:57:00', duration: '00:49:00' },
+               { from_airport: KCI, to_airport: DIA,
+                departure_time: '2020-12-15 08:23:00', duration: '01:22:00' },
+              ])
